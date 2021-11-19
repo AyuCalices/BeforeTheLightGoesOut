a3269b3e0e1383ae617e3bbb344454bedf1b0519
lukasGlas <s0571104@htw-berlin.de>
2021-11-19 20:28:58 +0100

    map kind of appears now
    it's generating

--- GreatMeadow/Assets/Features/Map_Namespace/MapController.cs
+++ GreatMeadow/Assets/Features/Map_Namespace/MapController.cs
@@ -9,12 +9,36 @@ using UnityEngine.SceneManagement;
 using Utils.Variables_Namespace;
 using Random = UnityEngine.Random;
 using Scene = UnityEditor.SearchService.Scene;
+using Features.Maze_Namespace.Tiles;
+
+
 
 public class MapController : MonoBehaviour
 {
+    [Tooltip("Width of generated maze in number of tiles (x-axis).")]
+    [SerializeField] private IntVariable width;
+    [Tooltip("Height of generated maze in number of tiles (y-axis).")]
+    [SerializeField] private IntVariable height;
+
+    [SerializeField] private Transform mapCanv;
+    [SerializeField] private MiniMapTileGenerator_SO mapTile;
+    
+    private void DrawTiles()
+    {
+        for (int y = 0; y < height.intValue; y++)
+        {
+            for (int x = 0; x < width.intValue; x++)
+            {
+              
+                Vector2Int gridPosition = new Vector2Int(x, y);
+                mapTile.InstantiateTileAt(gridPosition, mapCanv);
+            }
+        }
+    }
     
     private void Awake()
     {
+        DrawTiles();
 
     }
 
