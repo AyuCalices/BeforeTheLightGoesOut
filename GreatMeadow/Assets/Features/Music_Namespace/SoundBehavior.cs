fee45b0bcd427f2851d6be8a3de9f1ecc21b3e62
AyuCalices <58752514+AyuCalices@users.noreply.github.com>
2021-12-08 20:10:46 +0100

    implemented sound regulation and main menu sounds

--- GreatMeadow/Assets/Features/Music_Namespace/SoundBehavior.cs
+++ GreatMeadow/Assets/Features/Music_Namespace/SoundBehavior.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -29,6 +30,31 @@ namespace Sound_Namespace.Logic
             }
             soundTracks[currentTrackIndex].Play();
             Invoke(nameof(PlayNextTrack), soundTracks[currentTrackIndex].clip.length);
+        }
+        
+        private IEnumerator FadeTrack(AudioSource audioSource, float toVal, float duration)
+        {
+            float counter = 0f;
+            float startVolume = audioSource.volume;
+
+            while (counter < duration)
+            {
+                if (Time.timeScale == 0)
+                    counter += Time.unscaledDeltaTime;
+                else
+                    counter += Time.deltaTime;
+
+                audioSource.volume = Mathf.Lerp(startVolume, toVal, counter / duration);
+                yield return null;
+            }
+            
+            gameObject.SetActive(false);
+            audioSource.volume = startVolume;
+        }
+
+        public void Disable()
+        {
+            FadeTrack(soundTracks[currentTrackIndex], 0, 1);
         }
     }
 }
