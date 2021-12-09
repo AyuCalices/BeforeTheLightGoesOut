cff1458d5118f5b9cb7040dc3b5f10bbf69e26b9
AyuCalices <58752514+AyuCalices@users.noreply.github.com>
2021-12-09 22:04:22 +0100

    renamed project

--- GreatMeadow/Assets/Features/Maze_Namespace/Modifier/MazeModifier.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Features.Maze_Namespace;
-using UnityEngine;
-
-public abstract class MazeModifier : ScriptableObject
-{
-    public abstract void AddInteractableModifier(MazeGenerator_Behaviour mazeGenerator, List<TileBehaviour> tilesWithoutInteractable);
-}
