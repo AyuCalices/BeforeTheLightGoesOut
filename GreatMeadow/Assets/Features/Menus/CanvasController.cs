22c8e30a2379601e44397e565fb851268263d43e
AyuCalices <58752514+AyuCalices@users.noreply.github.com>
2021-12-09 22:07:30 +0100

    removed old project name folder

--- GreatMeadow/Assets/Features/Menus/CanvasController.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CanvasController : MonoBehaviour
-{
-    public MenuType_SO canvasType;
-    public bool isStartMenu;
-}
--- GreatMeadow/Assets/Utils/StateMachine_Namespace/IStateManaged.cs
+++
@@ -1,8 +0,0 @@
-
-namespace Utils.StateMachine_Namespace
-{
-    public interface IStateManaged
-    {
-        void RequestState(IState requestedState);
-    }
-}
